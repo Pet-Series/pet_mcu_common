127e7284d5af0b1d0f0749efb294a08ecf9a7157
kullken <viktorku@kth.se>
2021-10-24 15:24:19 +0200

    doc: Add comment for maximum number of modules (PET-4)

--- core/include/modules.h
+++ core/include/modules.h
@@ -6,7 +6,9 @@
 namespace pet
 {
 
+/// Maximum number of modules we support. Current maximum on vehicle: 1 Engine + 1 UltraSound + 3 LineSensor = 5 on Pet-Mk-IV Uno.
 constexpr int kMaxNumModules = 5;
+
 extern Timer<kMaxNumModules> g_timer;
 
 enum class ConfigResult
