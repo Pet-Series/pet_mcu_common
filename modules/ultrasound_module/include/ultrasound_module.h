4ba5572bd8b537422cdd8941c1dd6207def6a29a
kullken <karl.viktor.kull@gmail.com>
2022-04-09 18:57:44 +0200

    feat: Publish ultrasound data on multiple topics (PET-5)

--- modules/ultrasound_module/include/ultrasound_module.h
+++ modules/ultrasound_module/include/ultrasound_module.h
@@ -19,13 +19,20 @@ constexpr float deg2rad(float degrees)
     return degrees * M_PI / 180.0f;
 }
 
+class DefaultConstructablePublisher : public ros::Publisher
+{
+public:
+    DefaultConstructablePublisher() : Publisher("dummy_topic", nullptr) {}
+
+    using Publisher::Publisher;
+};
+
 template<int kSensorCount>
 class UltrasoundModule : public ArduinoModule
 {
 private:
     static constexpr double kFrequency = 30;
     static const ros::Duration kPeriod;
-    static constexpr auto kTopicName = "dist_sensors";
 
 public:
     UltrasoundModule(const int pins[kSensorCount], const char* const sensor_ids[kSensorCount]);
@@ -34,7 +41,7 @@ public:
 
 private:
     sensor_msgs::Range m_msg{};
-    ros::Publisher m_publisher;
+    DefaultConstructablePublisher m_publishers[kSensorCount];
 
     int m_current_sensor = 0;
     Ultrasound m_sensors[kSensorCount];
@@ -44,8 +51,8 @@ template<int kSensorCount>
 const ros::Duration UltrasoundModule<kSensorCount>::kPeriod = ros::Duration{1.0/kFrequency};
 
 template<int kSensorCount>
-UltrasoundModule<kSensorCount>::UltrasoundModule(const int pins[kSensorCount], const char* const sensor_ids[kSensorCount])
-    : m_publisher(kTopicName, &m_msg)
+UltrasoundModule<kSensorCount>::UltrasoundModule(const int pins[kSensorCount], 
+                                                 const char* const sensor_ids[kSensorCount])
 {
     m_msg.radiation_type = sensor_msgs::Range::ULTRASOUND;
     m_msg.field_of_view  = deg2rad(50);
@@ -53,11 +60,12 @@ UltrasoundModule<kSensorCount>::UltrasoundModule(const int pins[kSensorCount], c
     m_msg.max_range      = Ultrasound::kMaxDistance_cm / 100.0f;
 
     for (int i = 0; i < kSensorCount; ++i) {
-        m_sensors[i] = Ultrasound{pins[i], pins[i], sensor_ids[i]};
+        m_sensors[i]    = Ultrasound{pins[i], pins[i], sensor_ids[i]};
+        m_publishers[i] = DefaultConstructablePublisher{m_sensors[i].topic(), &m_msg};
+        pet::nh.advertise(m_publishers[i]);
     }
 
     m_sensors[m_current_sensor].start_ping();
-    pet::nh.advertise(m_publisher);
 }
 
 template<int kSensorCount>
@@ -71,8 +79,7 @@ ros::Time UltrasoundModule<kSensorCount>::callback(const TimerEvent& event)
     m_msg.header.frame_id = current_sensor.frame_id();
     m_msg.range           = current_sensor.get_distance();
 
-    m_publisher.topic_ = current_sensor.topic();
-    m_publisher.publish(&m_msg);
+    m_publishers[m_current_sensor].publish(&m_msg);
 
     m_current_sensor = (m_current_sensor + 1) % kSensorCount;
     m_sensors[m_current_sensor].start_ping();
